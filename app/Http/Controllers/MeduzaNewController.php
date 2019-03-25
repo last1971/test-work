16cef388f0507f6fa79165a44b3679af226285d7
vladimir <elcopro@gmail.com>
2019-03-25 22:04:44 +0300

    Add Comments

--- app/Http/Controllers/MeduzaNewController.php
+++ app/Http/Controllers/MeduzaNewController.php
@@ -57,7 +57,10 @@ class MeduzaNewController extends Controller
         }
         return response()->json([ 'success' => true ]);
     }
-
+    /*
+     * GET /news/list?day=<YYYY-MM-DD>[&document_type=<DOCUMENT_TYPE>]
+     * Запрос новостей за день из базы данных.
+     */
     public function list(Request $request)
     {
         try
@@ -74,24 +77,31 @@ class MeduzaNewController extends Controller
             return response()->json([ 'success' => false, 'error' => $e->getMessage() ], 500);
         }
     }
-
+    /*
+     * GET /news/image/<new-id>
+     * Запрос на картинку новости.
+     */
     public function image($type, $id, $year = null, $month = null, $day = null)
     {
-        $meduzaNew = $year
-            ? MeduzaNew::find($type . '/' . $id . '/'. $year . '/' . $month . '/' . $day)
-            : MeduzaNew::find($type . '/' . $id);
-        $error = 'News with this Id is missing';
-        if ($meduzaNew)
-        {
-            if ($meduzaNew->image)
-            {
-                $from = strrpos($meduzaNew->image, '/') + 1;
-                $name = substr($meduzaNew->image, $from);
-                Image::make('https://meduza.io' . $meduzaNew->image)->save($name);
-                return response()->download($name)->deleteFileAfterSend();
+        try {
+            $meduzaNew = $year
+                ? MeduzaNew::find($type . '/' . $id . '/' . $year . '/' . $month . '/' . $day)
+                : MeduzaNew::find($type . '/' . $id);
+            $error = 'News with this Id is missing';
+            if ($meduzaNew) {
+                if ($meduzaNew->image) {
+                    $from = strrpos($meduzaNew->image, '/') + 1;
+                    $name = substr($meduzaNew->image, $from);
+                    Image::make('https://meduza.io' . $meduzaNew->image)->save($name);
+                    return response()->download($name)->deleteFileAfterSend();
+                }
+                $error = 'News without a picture';
             }
-            $error = 'News without a picture';
+            return response()->json(['success' => false, 'error' => $error], 422);
         }
-        return response()->json([ 'success' => false, 'error' => $error ], 422);
+        catch (\Exception $e)
+        {
+            return response()->json([ 'success' => false, 'error' => $e->getMessage() ], 500);
+        }
     }
 }
